00c9db96faefff7096ce7f6de3bcce3e423fcc03
alex100588 <49831245+alex100588@users.noreply.github.com>
2023-03-19 08:56:41 +0200

    Update Alert.tsx

--- react-app/src/components/Alert.tsx
+++ react-app/src/components/Alert.tsx
@@ -1,21 +1,23 @@
-import { ReactNode } from "react"
+import { ReactNode, useState } from "react"
 
 interface Props{
     children: ReactNode,
+    onClose: ()=>void
 }
 
-const Alert = ({children}: Props) => {
+const Alert = ({children, onClose}: Props) => {
   return (
     <>
-    <div className="alert alert-primary text-center" role="alert">
+    <div className="alert alert-primary text-center alert-dismissible" role="alert">
         {children}
+        <button onClick={onClose} type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
       </div>
-    <div className="alert alert-secondary text-center" role="alert">
+    {/* <div className="alert alert-secondary text-center" role="alert">
         {children}
       </div>
     <div className="alert alert-danger text-center" role="alert">
         {children}
-      </div>
+      </div> */}
       </>
   )
 }
