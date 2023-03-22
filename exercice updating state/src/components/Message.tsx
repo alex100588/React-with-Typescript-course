78dc46cf9eb776de08e127ef0ca200e42f9ae939
alex100588 <49831245+alex100588@users.noreply.github.com>
2023-03-22 16:42:54 +0200

    Update Message.tsx

--- exercice updating state/src/components/Message.tsx
+++ exercice updating state/src/components/Message.tsx
@@ -1,14 +1,17 @@
-import React from 'react'
+import ExpandableText from "./components/ExpandableText";
 
-let count = 0
-
-const Message = () => {
-    count++
-    console.log(count);
-    
-    return (
-        <div>Message {count}</div>
-  )
+function App() {
+  
+  return (
+    <>
+    <div>
+      <ExpandableText maxChars = {10} >
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt recusandae ducimus quisquam in ab cumque! Perferendis, ratione tempora consequuntur eaque maiores voluptates, debitis nulla iste ullam modi ipsa a itaque, porro dicta. Repellendus aut sit voluptate nesciunt, dicta fuga? Necessitatibus voluptatibus eveniet harum voluptatem alias iste sequi? Hic quo excepturi, mollitia quasi vel explicabo quis nulla sequi suscipit unde voluptatibus nisi praesentium temporibus quaerat iste architecto aspernatur minus provident et. Voluptatibus laborum dolorem obcaecati exercitationem corporis sit in eos nihil sapiente repellat suscipit, fugit reprehenderit atque illo praesentium facilis! Labore iste aliquam fuga accusamus nulla eum aspernatur necessitatibus sapiente provident?
+      </ExpandableText>
+      
+    </div>
+    </>
+  );
 }
 
-export default Message
+export default App;
