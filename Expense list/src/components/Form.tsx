742ee3520b79ee8fca5d979153d4e3363a0ebb10
alex100588 <49831245+alex100588@users.noreply.github.com>
2023-03-29 15:00:13 +0300

    Update Form.tsx

--- Expense list/src/components/Form.tsx
+++ Expense list/src/components/Form.tsx
@@ -1,22 +1,30 @@
 import React from "react";
+import { categories } from "../App";
+
 interface Props {}
 
 const Form = () => {
   return (
     <form action="">
       <div className="mb-3">
-        <label htmlFor="name" className="form-label">Description</label>
-        <input type="text" id="name" className="form-control" />
+        <label htmlFor="description" className="form-label">Description</label>
+        <input type="text" id="description" className="form-control" />
       </div>
 
       <div className="mb-3">
-        <label htmlFor="name" className="form-label">Amount</label>
-        <input type="text" id="name" className="form-control" />
+        <label htmlFor="amount" className="form-label">Amount</label>
+        <input type="text" id="amount" className="form-control" />
       </div>
 
       <div className="mb-3">
-        <label htmlFor="name" className="form-label">Category</label>
-        <input type="text" id="name" className="form-control form-select" />
+        <label htmlFor="category" className="form-label">Category</label>
+        <select name="category" id="" className="form-select">
+        {categories.map(category =>{
+          return (
+            <option key={category} value={category}>{category}</option>
+          )
+        })}
+        </select>
       </div>
       
       <button className="btn btn-primary mb-3">Submit</button>
