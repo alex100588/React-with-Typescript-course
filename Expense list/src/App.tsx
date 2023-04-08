b48bab10f0659fef431c93a93f77a07a12b13d9a
alex100588 <49831245+alex100588@users.noreply.github.com>
2023-04-08 15:21:03 +0300

    Update App.tsx

--- Expense list/src/App.tsx
+++ Expense list/src/App.tsx
@@ -1,39 +1,39 @@
 import { useState } from "react";
-import ExpenseFilter from "./components/ExpenseFilter";
 import ExpenseList from "./components/ExpenseList";
-import Form from "./components/Form";
-
-export const categories = ['Groceries', 'Utilities', 'Entertainmnet']
+import { ExpenseFilter } from "./components/ExpenseFilter";
+import ExpenseForm from "./components/ExpenseForm";
+import categories from "./categories";
 
 function App() {
-  const[selectedCategory, setSelectedCategory] = useState('')
-  const [expenses, setExpenses] = useState([
+  const [selectedCategory, setSelectedCategory] = useState("");
+  const [expeses, setExpenses] = useState([
     { id: 1, description: "sss", amount: 10, category: "Groceries" },
     { id: 2, description: "dd", amount: 10, category: "Utility" },
     { id: 3, description: "ssaaas", amount: 20, category: "Utility" },
     { id: 4, description: "ggg", amount: 10, category: "Utility" },
   ]);
 
-  const visibleExpenses = selectedCategory ? expenses.filter(e => e.category === selectedCategory) : expenses
-
+  const visibleExpenses = selectedCategory
+    ? expeses.filter((e) => e.category === selectedCategory)
+    : expeses;
 
   return (
-    <>
-      <div>
-        <Form />
-        <div className="mb-5">
-          <ExpenseFilter
-            onSelectCategory={category => setSelectedCategory(category)}
-          />
-        </div>
-        <ExpenseList
-          expenses={visibleExpenses}
-          onDelete={(id) =>
-            setExpenses(expenses.filter((exp) => exp.id !== id))
-          }
+    <div>
+      <div className="mb-5">
+        <ExpenseForm />
+      </div>
+      <div className="mb-3">
+        <ExpenseFilter
+          onSelectCategory={(category) => setSelectedCategory(category)}
         />
       </div>
-    </>
+      <ExpenseList
+        expeses={visibleExpenses}
+        onDelete={(id) =>
+          setExpenses(expeses.filter((expense) => expense.id !== id))
+        }
+      />
+    </div>
   );
 }
 
