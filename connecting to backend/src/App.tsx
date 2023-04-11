6d5eaaa11151d13343c02aac8f67bbd753c81404
alex100588 <49831245+alex100588@users.noreply.github.com>
2023-04-11 13:43:14 +0300

    Update App.tsx

--- connecting to backend/src/App.tsx
+++ connecting to backend/src/App.tsx
@@ -1,25 +1,47 @@
 import { useEffect, useRef, useState } from "react";
+import axios from "axios";
 // import ProductList from "./components/ProductList";
 
+interface User {
+  id: number;
+  name: string;
+}
+
 function App() {
-  // const [category, setCategory] = useState('')
-  const connect = () => console.log("Connected");
-  const disconnect = () => console.log("Disconnected");
+  const [users, setUsers] = useState<User[]>([]);
 
   useEffect(() => {
-    connect();
-    return () => disconnect();
-  });
+    axios
+      .get<User[]>("https://jsonplaceholder.typicode.com/users")
+      .then((resp) => setUsers(resp.data));
+  }, []);
+  // console.log(users);
+
+  // const [category, setCategory] = useState('')
+
+  // Effect cleanup
+  // const connect = () => console.log("Connected");
+  // const disconnect = () => console.log("Disconnected");
+
+  // useEffect(() => {
+  //   connect();
+  //   return () => disconnect();
+  // });
 
   return (
-    <div>
-      {/* <select name="" id="" className="form-select" onChange={(e)=>setCategory(e.target.value)}>
-      <option value=""></option>
-      <option value="clothing">Clothing</option>
-      <option value="household">Household</option>
-    </select>
-    <ProductList category={category} /> */}
-    </div>
+    // <div>
+    //   <select name="" id="" className="form-select" onChange={(e)=>setCategory(e.target.value)}>
+    //   <option value=""></option>
+    //   <option value="clothing">Clothing</option>
+    //   <option value="household">Household</option>
+    // </select>
+    // <ProductList category={category} />
+    // </div>
+    <ul>
+      {users.map((user) => (
+        <li key={user.id}>{user.name}</li>
+      ))}
+    </ul>
   );
 }
 
