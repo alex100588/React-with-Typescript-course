07454377b479fde6b9c4f15b24d9f511ab7f7ca0
alex100588 <49831245+alex100588@users.noreply.github.com>
2023-04-18 15:38:36 +0300

    Add files via upload

---
+++ game-hub/src/App.tsx
@@ -0,0 +1,26 @@
+import { Grid, GridItem, Show } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "asside main"`,
+      }}
+    >
+      <GridItem area="nav" bg="coral">
+        Nav
+      </GridItem>
+      <Show above="lg">
+        <GridItem area="asside" bg="gold">
+          Nav
+        </GridItem>
+      </Show>
+      <GridItem area="main" bg="dodgerblue">
+        Nav
+      </GridItem>
+    </Grid>
+  );
+}
+
+export default App;
