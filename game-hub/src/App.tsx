d5c89e838e0cea70a6fd36d6ff82f1723564c45c
alex100588 <49831245+alex100588@users.noreply.github.com>
2023-04-19 14:11:54 +0300

    Update App.tsx

--- game-hub/src/App.tsx
+++ game-hub/src/App.tsx
@@ -1,5 +1,6 @@
 import { Grid, GridItem, Show } from "@chakra-ui/react";
 import NavBar from "./components/NavBar";
+import GameGrid from "./components/GameGrid";
 
 function App() {
   return (
@@ -13,12 +14,12 @@ function App() {
         <NavBar />
       </GridItem>
       <Show above="lg">
-        <GridItem area="asside" bg="gold">
+        <GridItem area="asside" >
           Asside
         </GridItem>
       </Show>
-      <GridItem area="main" bg="dodgerblue">
-        Main
+      <GridItem area="main" >
+        <GameGrid />
       </GridItem>
     </Grid>
   );
