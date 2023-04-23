7c1c722ec58e796733df8c0a5eaff9d84bd14fb2
alex100588 <49831245+alex100588@users.noreply.github.com>
2023-04-23 11:36:02 +0300

    Update useGames.ts

--- game-hub/src/hooks/useGames.ts
+++ game-hub/src/hooks/useGames.ts
@@ -3,10 +3,17 @@ import apiClient from "../services/api-client";
 import { CanceledError } from "axios";
 import { background } from "@chakra-ui/react";
 
+export interface Platform {
+  id: number,
+  name: string,
+  slug: string
+}
+
 export interface Game {
   id: number;
   name: string;
   background_image: string;
+  parent_platforms: { platform: Platform }[]
 }
 
 interface FetchGamesResponse {
